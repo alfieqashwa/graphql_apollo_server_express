ae0878f9e32a01d786efb5f41ddcc741972ea27e
alfieqashwa <alfieqahwa@gmail.com>
2018-12-16 01:23:28 +0700

    technical separation

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Qashwa Zhafira',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'Cello el Zhafran',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Cello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+export deafult {
+    users,
+    messages,
+}
