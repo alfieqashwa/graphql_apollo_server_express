bdc9cbd6b7f989215210dcd489de4a67890e337e
alfieqashwa <alfieqahwa@gmail.com>
2018-12-15 22:45:06 +0700

    Finished Apollo Server: Type Relationships

--- src/index.js
+++ src/index.js
@@ -1,13 +1,105 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const app = express();
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
+  }
+`;
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Alfie Qashwa',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'Qashwa Zhafira',
+    messageIds: [2],
+  },
 };
 
-console.log(user);
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
-console.log(process.env.SOME_ENV_VARIABLE);
+const resolvers = {
+  Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: (parent, args, { me }) => {
+      return me;
+    },
+    messages: () => {
+      return Object.values(messages);
+    },
+    message: (parent, { id }) => {
+      return messages[id];
+    },
+  },
+
+  User: {
+    messages: user => {
+      return Object.values(messages).filter(
+        message => message.userId === user.id,
+      );
+    },
+  },
+
+  Message: {
+    user: message => {
+      return users[message.userId];
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    me: users[1],
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
